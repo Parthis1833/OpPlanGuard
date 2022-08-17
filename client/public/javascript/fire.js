12ac9f108f9fafdc49353f978cc773d282d34481
Parthis <parthisj03@gmail.com>
2022-08-17 17:35:06 +0530

    singing hosting complet

--- client/public/javascript/fire.js
+++ client/public/javascript/fire.js
@@ -1,9 +1,13 @@
-import { initializeApp } from 'firebase/app';
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-analytics.js";
+  
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
     apiKey: "AIzaSyDJBF8sUiD6n_yl5O8IIX3jSAMZ2UdCjcI",
     authDomain: "gp-police.firebaseapp.com",
     databaseURL: "https://gp-police-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -13,6 +17,7 @@ const firebaseConfig = {
     appId: "1:399031635594:web:b670b1a298056e12f76773",
     measurementId: "G-FGPZD71DER"
   };
-const fire =firebaseConfig.initializeApp(firebaseConfig);
 
-export default fire;
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
