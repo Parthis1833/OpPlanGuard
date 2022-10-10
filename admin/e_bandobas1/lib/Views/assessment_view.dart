61da0bf08fbd31589c4e72106fcddd31faf88acf
Parthis1833 <parthisj03@gmail.com>
2022-10-10 21:20:05 +0530

    add device_preview 1.1.0

--- admin/e_bandobas1/lib/Views/assessment_view.dart
+++ admin/e_bandobas1/lib/Views/assessment_view.dart
@@ -14,13 +14,13 @@ class AssessmentView extends GetView<AssessmentController> {
     else if(width > 900){
       return 400;
     }
-    else if (width > 800){
+    else if (width <= 890){
       return 400;
     }
    else if( width >= 600){
      return 600;
     }
-    else if(width <= 600 && width > 480){
+    else if(width <= 640 && width > 480){
       return 600;
     }
     // all phones
--- admin/e_bandobas1/lib/main.dart
+++ admin/e_bandobas1/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:e_bandobas1/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:device_preview/device_preview.dart';
 
 
 void main() {
   runApp(
-    GetMaterialApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    ),
+    ),),
   );
 }
