f6be8da8160fc414455cf0bd0483bac2f6124a70
shyam kachhadiya <shyam1ss15@gmail.com>
2023-02-11 17:58:30 +0530

    adsbkasnfd

--- admin/get cli/e_bandobas/lib/app/modules/pointlist/views/pointlist_view.dart
+++ admin/get cli/e_bandobas/lib/app/modules/pointlist/views/pointlist_view.dart
@@ -2,6 +2,7 @@ import 'package:e_bandobas/app/modules/pointlist/views/PointViewDataGrid.dart';
 import 'package:e_bandobas/app/resource/card/PoliceCard.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../../Widgets/navigation_drawer.dart';
 import '../controllers/pointlist_controller.dart';
 
 class PointlistView extends GetView<PointlistController> {
@@ -9,6 +10,7 @@ class PointlistView extends GetView<PointlistController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const Navigation_Drawer(),
       appBar: AppBar(
         title: const Text('PointlistView'),
         centerTitle: true,
