afc2f3d772a9114122e956916bf7938d6a84b779
shyam kachhadiya <shyam1ss15@gmail.com>
2023-02-28 03:11:41 +0000

    mac os configurations

--- admin/get cli/e_bandobas/lib/app/modules/dutypointallocation/controllers/dutypointallocation_controller.dart
+++ admin/get cli/e_bandobas/lib/app/modules/dutypointallocation/controllers/dutypointallocation_controller.dart
@@ -9,6 +9,7 @@ import '../../../jsondata/EventPointAssignmentData/eventAssignmentModel.dart';
 import 'dart:io';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:open_file_plus/open_file_plus.dart';
+import 'package:http/http.dart' as http;
 
 class DutypointallocationController extends GetxController {
   late final selectedEventId = 0.obs;
@@ -16,6 +17,7 @@ class DutypointallocationController extends GetxController {
   final eventAssignmentModel = Rxn<EventAssignmentModel>();
 
   final count = 0.obs;
+
   @override
   void onInit() {
     super.onInit();
@@ -60,16 +62,66 @@ class DutypointallocationController extends GetxController {
   // }
 
   void downloadExcel() async {
-    final path = getApplicationDocumentsDirectory();
     if (eventAssignmentModel.value != null &&
         eventAssignmentModel.value!.fileName!.length > 0) {
-      final url = eventAssignmentModel.value!.fileName!;
-      print(url);
-      OpenFile.open(url);
-      // _launchUrl(url);
+      // File file =
+      //     await _getTemporaryDirectory(eventAssignmentModel.value!.fileName!);
+      // file.exists().then((bool exists) {
+      //   if (exists) {
+      //     file.readAsBytes().then((bytes) {
+      //       print("file exists");
+      //       final blob = html.Blob([bytes], 'application/vnd.ms-excel');
+      //       final url = html.Url.createObjectUrlFromBlob(blob);
+      //       html.window.open(url, "_self");
+      //     });
+      //   }
+      // });
+      // OpenFile.open(eventAssignmentModel.value!.fileName!);
+      // downloadFile(eventAssignmentModel.value!.fileName!);
+      // processFile(eventAssignmentModel.value!.fileName!);
+
+      print("file exists");
     } else {
       throw DataNotFoundException("Download file not found").errorSnackBar();
     }
   }
 
+  // Future<File> _getTemporaryDirectory(String filePath) async {
+  //   // Directory tempDir = await getTemporaryDirectory();
+  //   // String _tempPath = tempDir.path;
+  //   // String _tempFilePath = '$_tempPath/${DateTime.now().millisecondsSinceEpoch}.xls';
+  //   // return await File(filePath).copy(_tempFilePath);
+  //   return File(filePath);
+  // }
+
+  // void downloadFile(String url) async {
+  //   final filename = url.split('/').last;
+  //   final fileBytes = await html.HttpRequest.request(url, responseType: 'blob')
+  //       .then((value) => value.response);
+  //   final file = html.File([fileBytes], filename);
+  //   print(file.relativePath);
+  //   print(file.size);
+  //   print(file.name);
+  //   final downloadUrl = html.Url.createObjectUrl(file);
+  //   print(downloadUrl);
+  //   final anchorElement = html.AnchorElement(href: downloadUrl);
+  //   anchorElement.setAttribute('download', filename);
+  //   html.document.body!.append(anchorElement);
+  //   anchorElement.click();
+  //   html.Url.revokeObjectUrl(downloadUrl);
+  //   anchorElement.remove();
+  // }
+
+  // void processFile(String url) async {
+  //   final file = js.JsObject(js.context['File'], [url]);
+
+  //   // Create a new Object URL for the local file.
+  //   final urlObject = js.context.callMethod('URL.createObjectURL', [file]);
+
+  //   // Download the file.
+  //   downloadFile(urlObject);
+
+  //   // Revoke the Object URL after the download is complete.
+  //   js.context.callMethod('URL.revokeObjectURL', [urlObject]);
+  // }
 }
