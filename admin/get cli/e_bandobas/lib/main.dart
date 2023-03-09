d8645065ada96c082ecde9b9c38155ccb6beec49
Parthis Joshi <62930148+Parthis1833@users.noreply.github.com>
2023-03-09 15:41:25 +0530

    toggle switch update

--- admin/get cli/e_bandobas/lib/main.dart
+++ admin/get cli/e_bandobas/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:e_bandobas/app/Config/routes/app_pages.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:window_manager/window_manager.dart';
 
