6c6cf5b743405865d4ade13becf6e57d7ca1eed0
shyam kachhadiya <shyam1ss15@gmail.com>
2023-03-24 05:36:17 +0000

    added process execution code

--- admin/get cli/e_bandobas/lib/main.dart
+++ admin/get cli/e_bandobas/lib/main.dart
@@ -3,13 +3,36 @@ import 'package:e_bandobas/app/Config/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:window_manager/window_manager.dart';
+import 'dart:convert';
 
 void main() async {
+  // String path = "/Users/apple/Desktop/projects/gujarat_police_backend/";
+  String path = "/Users/apple/Desktop/projects/gujarat_police_backend/";
+  Process.run('curl', ['-X', 'POST', 'localhost:8080/actuator/shutdown'],
+          workingDirectory: path)
+      .then((ProcessResult results) {
+    print(results.stdout);
+    Process.run('git', ['checkout', 'development'], workingDirectory: path)
+        .then((ProcessResult results) {
+      Process.run('git', ['pull'], workingDirectory: path)
+          .then((ProcessResult results) {
+        Process.run('mvn', ['spring-boot:run'], workingDirectory: path)
+            .then((ProcessResult results) {
+          print(results.stdout);
+          startApp();
+        });
+      });
+    });
+  });
+}
+
+void startApp() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     WindowManager.instance.setFullScreen(true);
-    WindowManager.instance.setMaximumSize(const Size(double.infinity, double.infinity));
+    WindowManager.instance
+        .setMaximumSize(const Size(double.infinity, double.infinity));
     WindowManager.instance.setMinimumSize(const Size(1224, 768));
   }
   runApp(
